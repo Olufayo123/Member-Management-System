9ade9c4a0f09544dfa2e47362c9e28442fbaf8fe
victor-olufayo <victorolufayo@tekexperts571.onmicrosoft.com>
2024-12-11 06:07:52 +0100

    Initial commit

---
+++ Member Management System/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Member_Management_System
+{
+    static class Program
+    {
+                
+        [STAThread]
+        // The attribute specifies that the application's COM threading model is single-threaded apartment (STA), which is essential for proper communication with COM components in Windows Forms applications.
+        static void Main()
+        {
+            //First method to be executed when the program starts
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
